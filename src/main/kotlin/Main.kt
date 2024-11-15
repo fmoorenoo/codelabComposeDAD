3be6af2bc6b087545b22b4b124f001ed8822a7af
fmoorenoo <fermontemore@gmail.com>
2024-11-15 09:00:21 +0000

    Commit inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+fun App() {
+    MaterialTheme {
+        screen1()
+    }
+}
+
+@Composable
+fun screen1() {
+
+}
+
+@Composable
+fun screen2() {
+
+}
+
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
