1ea746c897cc85211902c082ed24eee2a83f6c15
fmoorenoo <fermontemore@gmail.com>
2024-11-15 09:09:34 +0000

    screen1 iniciada

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,42 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 fun App() {
     MaterialTheme {
-        screen1()
+        Surface{
+            screen1()
+        }
     }
 }
 
 @Composable
 fun screen1() {
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Bienvenido a la peor app del mercado!")
+        Button(onClick = {}) {
+            Text(text = "Entrar")
+        }
+    }
 }
 
 @Composable
